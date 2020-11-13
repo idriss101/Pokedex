5fbd131616e3520bb8450f27d57a1052320cffb9
Idriss Kone <idriss.kone@icloud.com>
2020-11-13 17:21:52 -0500

    final styles to project

--- src/Pokemon.js
+++ src/Pokemon.js
@@ -2,6 +2,7 @@ import axios from "axios";
 import React, { useState, useEffect } from "react";
 import LinearProgress from "@material-ui/core/LinearProgress";
 import { withStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
 import "./Pokemon.css";
 import ProgressBar from "./ProgressBar";
 import MoreInfo from "./MoreInfo";
@@ -18,6 +19,7 @@ const styles = () => ({
 function Pokemon(props) {
   const [pokemon, setPokemon] = useState({});
   const [loading, setLoading] = useState(true);
+  const [showMore, setShowMore] = useState(false);
 
   useEffect(() => {
     const getData = async () => {
@@ -44,6 +46,23 @@ function Pokemon(props) {
     }
   };
 
+  const MoreDetails = () => {
+    setShowMore(true);
+  };
+
+  const LessDetails = () => {
+    setShowMore(false);
+  };
+
+  // eslint-disable-next-line no-extend-native
+  Number.prototype.pad = function (size) {
+    var s = String(this);
+    while (s.length < (size || 3)) {
+      s = "0" + s;
+    }
+    return s;
+  };
+
   // console.log(pokemon);
 
   if (loading) {
@@ -57,11 +76,19 @@ function Pokemon(props) {
     <div className="Pokemon">
       <div className="Pokemon-main">
         <img src={pokemon.sprites.front_default} alt="" />
-        <div className="Pokemon-id">{/* <p>#001</p> */}</div>
+        <div className="Pokemon-id">
+          <p>#{pokemon.id.pad()}</p>
+        </div>
+        <div className="Pokemon-height">
+          <p>0.7m</p>
+        </div>
+        <div className="Pokemon-weight">
+          <p>6.9kg</p>
+        </div>
         <h2>{pokemon.name}</h2>
         <ul className="Pokemon-main--types">
           {pokemon.types.map((type) => (
-            <li>{type.type.name}</li>
+            <li key={type.type.name}>{type.type.name}</li>
           ))}
         </ul>
         <ul>
@@ -76,7 +103,7 @@ function Pokemon(props) {
           <tbody>
             {pokemon.stats.map((stat) => (
               <>
-                <tr>
+                <tr key={stat.stat.name}>
                   <td>{stat.stat.name}</td>
                   <td>{stat.base_stat}</td>
                   <td style={{ width: "100%" }}>
@@ -92,9 +119,36 @@ function Pokemon(props) {
           </tbody>
         </table>
       </div>
-      <div className="Pokemon-more-info">
-        <MoreInfo pokemon={pokemon} />
-      </div>
+      {showMore ? (
+        <div className="Pokemon-more-info">
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={LessDetails}
+            style={{
+              backgroundColor: "#70C6C7",
+              margin: "10px",
+              fontWeight: "700",
+            }}
+          >
+            Less Details
+          </Button>
+          <MoreInfo pokemon={pokemon} />
+        </div>
+      ) : (
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={MoreDetails}
+          style={{
+            backgroundColor: "#70C6C7",
+            margin: "10px",
+            fontWeight: "700",
+          }}
+        >
+          More Details
+        </Button>
+      )}
     </div>
   );
 }
